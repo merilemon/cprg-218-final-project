8e28e70f30e342eb6562f7b4c89e63019d668a40
merilemon <147794695+merilemon@users.noreply.github.com>
2024-04-21 19:09:28 -0600

    Initial commit

---
+++ js/java.js
@@ -0,0 +1,21 @@
+/* accordion */
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    } 
+  });
+}
+
+document.getElementById('yr').innerHTML = new Date().getFullYear();
+
+/* footer date */
+
